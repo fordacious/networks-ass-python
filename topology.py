545bc8aca02ebc15fb4ddee5e91677f978f4ff60
Lachlan Ford <fordyfor@hotmail.com>
2013-10-09 23:05:12 +1100

    made connections work

--- topology.py
+++ topology.py
@@ -34,20 +34,67 @@ class Topology(object):
         self.edges[(vert_from, vert_to)] = new_edge
         self.edges[(vert_to, vert_from)] = new_edge
 
-    def add_connection (vert_from, vert_to, time, time_to_live):
-        self.edges[(vert_from, vert_to)]["connections"] = {
+    def add_connection (self, vert_from, vert_to, time, time_to_live):
+        self.edges[(vert_from, vert_to)]["connections"].append( {
                 "activation_time" : time,
                 "time_to_live"    : time_to_live
-            }
+            } )
 
-    def clear_obsolete_connections (cur_time):
+    # adds connections along a path
+    def add_connection_path (self, path, time, time_to_live):
+        if len(path) == 2:
+            self.add_connection(path[0], path[1], time, time_to_live)
+        else:
+            self.add_connection(path[0], path[1], time, time_to_live)
+            self.add_connection_path(path[1:], time, time_to_live)
+
+    def clear_obsolete_connections (self, cur_time):
         for ek,edge in self.edges.items():
             # make a new list of connections with only the connections that should be active (the time has not exceeded their lifetime)
             # TODO possible fencepost error here
-            edge["connections"] = [con for ec,con in edge["connections"].items() if con["activation_time"] + con["time_to_live"] > cur_time]
+            edge["connections"] = [con for con in edge["connections"] if con["activation_time"] + con["time_to_live"] > cur_time]
                 
 
 if __name__ == '__main__':
     topology = Topology('topology.txt')
     print topology.vertices, topology.edges
     # TODO test adding and removing connections
+    '''
+    0.221267 B F 0.973490
+    1.379235 K D 2.095500
+    1.670436 D E 1.856995
+    2.286279 F L 39.597595
+    2.487471 O H 6.599911
+    2.634504 E M 22.582966
+    '''
+    print "TESTING CONNECTIONS"
+    topology.clear_obsolete_connections(0)
+    topology.add_connection_path(['B', 'C', 'D', 'F'], 0.221267, 1.973490)
+    print topology.edges[('B','C')]
+    print topology.edges[('C','D')]
+    print topology.edges[('D','F')]
+    topology.clear_obsolete_connections(1.379235)
+    topology.add_connection_path(['K', 'N', 'O', 'F', 'D'], 1.379235, 2.095500)
+    print topology.edges[('B','C')]
+    print topology.edges[('C','D')]
+    print topology.edges[('D','F')]
+    topology.clear_obsolete_connections(1.670436)
+    topology.add_connection_path(['D','E'], 1.670436, 1.856995)
+    print topology.edges[('B','C')]
+    print topology.edges[('C','D')]
+    print topology.edges[('D','F')]
+    topology.clear_obsolete_connections(2.286279)
+    topology.add_connection_path(['F', 'O', 'N', 'M', 'L'], 2.286279, 39.597595)
+    print topology.edges[('N','M')]
+    print topology.edges[('C','D')]
+    print topology.edges[('D','F')]
+    topology.clear_obsolete_connections(2.487471)
+    topology.add_connection_path(['O', 'G', 'H'], 2.487471, 6.599911)
+    print topology.edges[('B','C')]
+    print topology.edges[('O','G')]
+    print topology.edges[('D','F')]
+    topology.clear_obsolete_connections(2.634504)
+    topology.add_connection_path(['E', 'F', 'O', 'N', 'M'], 2.634504, 22.582966)
+    print topology.edges[('O','N')]
+    print topology.edges[('C','D')]
+    print topology.edges[('D','F')]
