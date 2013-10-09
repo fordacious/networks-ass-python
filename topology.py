77d0bc852b1d6374385a8bc592fbc9749d90b7e6
Benjamin James Wirght <bwright@cse.unsw.edu.au>
2013-10-09 15:35:40 +1100

    Basic implementation of topolgy completed

--- topology.py
+++ topology.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 
+from collections import defaultdict
+
 class Topology(object):
     '''
     Our datastructure that defines our topology.
     '''
     def __init__(self, topology_file):
-        pass
+        self.vertices = set()
+        self.edges = defaultdict(list)
+        self.weights  = {}
+
+    def add_vertex(self, value):
+        self.vertices.add(value)
+
+    def add_edge(self, vert_from, vert_to, weight):
+        self.edges[vert_from].append(vert_to)
+        self.edges[vert_to].append(vert_from)
+        self.weights[(vert_from, vert_to)] = weight
