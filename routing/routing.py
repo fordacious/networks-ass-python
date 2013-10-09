de0937d815023de8c3048ff8ff868bba9efa0c45
Lachlan Ford <fordyfor@hotmail.com>
2013-10-09 15:47:09 +1100

    routing can output and vars to keep stats

--- routing/routing.py
+++ routing/routing.py
@@ -8,3 +8,12 @@ class Routing(object):
     '''
     def __init__(self, topology):
         self.topology = topology
+        self.num_vc_requests = 0.0
+        self.num_blocked = 0.0
+	
+    def output (self):
+        print 'total number of virtual circuit requests: {0}'.format(num_vc_requests)
+        print 'number of successfully routed requests: {0}'.format(num_vc_requests - num_blocked)
+        print 'percentage of successfully routed request: {0}'.format((num_vc_requests - num_blocked) * 100.0 / num_vs_requests)
+        print 'number of blocked requests: {0}'.format(num_blocked)num_vc_requests - num_blocked
+        print 'percentage of blocked requests: {0}'.format(num_blocked * 100.0 / num_vc_requests)
