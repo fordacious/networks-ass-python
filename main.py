46797c2836eeb41cf77fdbbb10cb317d2f59cfbd
Lachlan Ford <fordyfor@hotmail.com>
2013-10-09 15:10:34 +1100

    Fancshuns

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python
 
-print "working"
+class Topology (object):
+	def __init__ (self, topData):
+		
+
+# gets topology from standard input
+def getTopology ():
+	pass
+
+def runSHP ():
+	pass
+
+def runSDP ():
+	pass
+
+def runLLP ():
+	pass
+
+top = Topology(getTopology())
+
+
